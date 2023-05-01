622af1c48d46cd9954ae3403b688ff063ac56040
Milagros-Trejo <82959873+Milagros-Trejo@users.noreply.github.com>
2023-05-01 14:15:23 -0300

    Primer iteracion de has_one

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -1,6 +1,18 @@
 class Prueba
-
   def materia
     :tadp
+  end
+end
+
+class Class
+  def has_one(tipo, descripcion)
+    # donde tipo es un tipo básico
+    # y descripción es un hash con el nombre del atributo
+    self.define_method(descripcion[:named]) do
+      self.instance_variable_get("@"+descripcion[:named].to_s)
+    end
+    self.define_method(descripcion[:named].to_s+"=") do |valor|
+      self.instance_variable_set(("@"+descripcion[:named].to_s).to_sym,valor)
+    end
   end
 end
