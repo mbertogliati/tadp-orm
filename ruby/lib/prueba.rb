d0d15489173bc7b8b9b96447b94281492504c5b0
Mateo Bertogliati <mbertogliati@frba.utn.edu.ar>
2023-05-05 23:00:33 -0300

    Hecho hasta el punto 2a)
    @table es ahora un atributo de las clases persistibles (antes todas las instancias tenian una @table).
    Ahora cada vez que se invoca 'has_one' en una clase persistible, se guarda en el atributo un objeto de tipo AtributoPersisible, el cual a su vez guarda el tipo y el valor. Los getters y setters de los atributos persisitibles no cambian para nada.

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -2,40 +2,106 @@ require 'tadb'
 
 module Persistible
   # punto 1 - a
+  private def id=(id)
+    @id = id
+  end
+  def cargar_con_hash(hash)
+    self.definir_metodo_id
+    hash.each do |key, value|
+      self.send(key.to_s+"=", value)
+    end
+    self
+  end
+  def save!
 
-  def initialize()
-    @tabla = TADB::DB.table(self.class.to_s)
+    atributos_persisitbles = self.instance_variables.select do
+    |atributo| self.instance_variable_get(atributo).is_a? AtributoPersistible
+    end
+
+    hash_values = {}
+
+    atributos_persisitbles.each do |atributo|
+      hash_values[atributo.to_s.gsub("@","").to_sym] = self.instance_variable_get(atributo).valor
+    end
+
+    #puts hash_values
+
+    self.definir_metodo_id
+
+    self.id= self.class.table.insert(hash_values)
+
   end
 
-  def save!
-    hash_values = {}
-    id = @tabla.insert(hash_values)
-    self.instance_variable_set("@id".to_sym, id)
+
+
+  def definir_metodo_id
     self.define_singleton_method("id") do
-      self.instance_variable_get(:@id)
+      @id
     end
   end
 
-  def refresh!
+  def borrar_metodo_id
+    self.singleton_class.remove_method("id")
+  end
 
+  def refresh!
+    self.borrar_tabla
+    self.save!
   end
 
   def forget!
-    @tabla.delete(self.id)
+    self.borrar_tabla
+    self.borrar_metodo_id
   end
 
+  private def borrar_tabla
+    self.class.table.delete(@id)
+  end
+
+end
+
+class AtributoPersistible
+  attr_accessor :tipo, :valor
+  def initialize(tipo, valor)
+    @tipo = tipo
+    @valor = valor
+  end
+
+  def valor= (valor)
+    @valor = valor
+  end
 
 end
 
 module ClasePersistible
-  def has_one(tipo, descripcion)
-    self.define_method(descripcion[:named]) do
-      self.instance_variable_get("@"+descripcion[:named].to_s)
-    end
-    self.define_method(descripcion[:named].to_s+"=") do |valor|
-      self.instance_variable_set(("@"+descripcion[:named].to_s).to_sym,valor)
-    end
+
+  def table
+    @table.nil? ? @table = TADB::DB.table(self.to_s) : @table
   end
+  def has_one(tipo, descripcion)
+
+    self.define_method(descripcion[:named]) do
+      atributo = self.instance_variable_get("@"+descripcion[:named].to_s)
+      atributo.valor
+    end
+
+    self.define_method(descripcion[:named].to_s+"=") do |valor|
+      self.instance_variable_set(("@"+descripcion[:named].to_s),AtributoPersistible.new(tipo, valor))
+    end
+
+  end
+
+  def all_instances
+    lista_hash = self.table.entries
+
+    lista_hash.map! do |hash|
+      instancia = self.new
+      instancia.cargar_con_hash(hash)
+    end
+
+    lista_hash
+  end
+
 end
 
 
@@ -46,9 +112,8 @@ end
 class Class
   def include(*args)
     if args.include? Persistible
-      / Le setea la singleton_class /
+      #Le setea la singleton_class
       self.extend(ClasePersistible)
-      self.instance_variable_set("@table", TADB::DB.table(self.to_s))
     end
     super
   end
@@ -64,9 +129,30 @@ class Person
 
 end
 
-class Main
-  /persona = Person.new
-  persona.first_name= "Thomy"
 
-  puts persona.save!/
+
+class Main
+  persona1 = Person.new
+  persona1.first_name= "Thomy"
+  persona1.last_name= "Pereyra"
+
+  persona2 = Person.new
+  persona2.first_name= "Milagros"
+  persona2.last_name= "Pereyra"
+
+  persona3 = Person.new
+  persona3.first_name= "Mateo"
+  persona3.last_name= "Pereyra"
+
+  #puts Person.table
+
+  persona1.save!
+  persona2.save!
+  persona3.save!
+
+  puts Person.all_instances
+
+
+  #puts Person.table.instance_variable_get(:@name)
+
 end
