b67017c9b46209c014059e1200ccce07bd9b7baa
Mateo Bertogliati <mbertogliati@frba.utn.edu.ar>
2023-06-17 13:40:48 -0300

    Refactor de codigo de parser

--- scala/src/main/scala/Prueba.scala
+++ scala/src/main/scala/Prueba.scala
@@ -2,10 +2,28 @@ import scala.util.{Try, Success, Failure}
 
 case class ResultadoParser[T](parseado: T, resto: String)
 
-object anyChar {
-  def apply(s: String): Try[ResultadoParser[Char]] = {
-    Try[ResultadoParser[Char]] {
-      new ResultadoParser[Char](s.head, s.tail)
+abstract class Parser[T]{
+  def parseado(s:String) : T
+  def resto(s:String) : String
+  def condicion(s : String) : Boolean
+  var mensaje : String
+
+  def apply(s: String): Try[ResultadoParser[T]] =
+    Try[ResultadoParser[T]] {
+      if(condicion(s)) {
+        new ResultadoParser[T](parseado(s), resto(s))
+      } else {
+        throw new Exception(mensaje)
+      }
     }
-  }
+}
+
+abstract class charParser extends Parser[Char]{
+  override def parseado(s:String) : Char = s.head
+  override def resto(s:String) : String = s.tail
+}
+
+object anyChar extends charParser {
+  def condicion(s : String) : Boolean = s.nonEmpty
+  var mensaje : String = "Cadena vacía"
 }
